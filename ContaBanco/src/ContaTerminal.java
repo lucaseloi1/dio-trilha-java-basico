d0489f167d2a60e2ae551d252c7c1a412ffb571e
lucaseloi1 <devleloi@outlook.com>
2025-11-21 22:17:42 -0300

    meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try {
+            Scanner sc = new Scanner(System.in);
+            double saldo = 237.48;
+
+            System.out.print("Digite o número da Conta: ");
+            int numero = sc.nextInt();
+
+            System.out.print("Digite o número da Agência: ");
+            String agencia = sc.next();
+
+            System.out.print("Digite o seu Nome: ");
+            String nome = sc.next();
+
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                    + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        } catch (java.util.InputMismatchException e) {
+            System.err.println(
+                    "\nErro: O número da conta deve ser um valor numérico inteiro. Por favor, execute o programa novamente.");
+        }
     }
 }
